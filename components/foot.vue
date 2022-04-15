bba7edd4f8d10605fcdb3cf0d399765799c1e2ea
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-15 16:56:45 +0900

    write something on foot.vue

--- components/foot.vue
+++ components/foot.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <footer class="footer">
+    <p>
+      This page is not University of Tsukuba's official page.
+    </p>
+    <p>
+      This page's author accept no responsibility for any loss resulting from using this page.
+    </p>
+  </footer>
 </template>
 
 <style>
