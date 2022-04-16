f9d26a79bc893df568493aeabcdcc49631250e6a
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-16 23:35:16 +0900

    add twin:te eritan

--- components/Applinks.vue
+++ components/Applinks.vue
@@ -1,6 +1,27 @@
 <template>
-  <div id="applinks">
-    筑波大学関連アプリの紹介を書く
+  <div id="applinks" class="m-2">
+    <div id="twinte">
+      <h2 class="title">Twin:te</h2>
+      <ul>
+        <li><a href="https://www.twinte.net/">公式サイト</a></li>
+        <li><a href="https://apps.apple.com/app/twin-te/id1489274755">AppStore</a></li>
+        <li><a href="https://play.google.com/store/apps/details?id=net.twinte.android">Google Play</a></li>
+      </ul>
+      <p>
+        筑波大学生向けの時間割ソフトです。Twinsからのインポート機能もあります。
+      </p>
+    </div>
+    <div id="eritan">
+      <h2 class="title">えりたんBOT</h2>
+      <ul>
+        <li><a href="https://eritanbot.net/">公式サイト</a></li>
+        <li><a href="https://itunes.apple.com/app/eritanbot/id865364505">AppStore</a></li>
+        <li><a href="https://play.google.com/store/apps/details?id=net.eritanbot.android">Google Play</a></li>
+      </ul>
+      <p>
+        広大なキャンパスの地図。キャンパス交通システムやTXの時刻表も。
+      </p>
+    </div>
   </div>
 </template>
 
