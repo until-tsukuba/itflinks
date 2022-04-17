8bd25e7dcbe04e3c8f9b40b06b815a11d5ba9353
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-17 23:02:11 +0900

    Revert "add links at footer"
    This reverts commit aedbc62d30d379438b72b59788a5c4d556f67804.

--- components/foot.vue
+++ components/foot.vue
@@ -7,10 +7,7 @@
       This page's author accept no responsibility for any loss resulting from using this page.
     </p>
     <p>
-      This site provides information on <nuxt-link to="/appli">appli</nuxt-link>, <nuxt-link to="/faq">faq</nuxt-link> and toppage.
-    </p>
-    <p>
-      link-ITF (<a href="https://itflinks.pages.dev">https://itflinks.pages.dev</a>) 2022.
+      link-ITF (<a href="https://itflinks.pages.dev">https://itflinks.pages.dev</a>) 2022
     </p>
   </footer>
 </template>
