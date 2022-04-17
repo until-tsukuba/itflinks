a21585da9a66ba1afdec4e980cc8b6790b69fe4a
Osumi Akari <kig.osumiakari@gmail.com>
2022-04-17 13:19:12 +0900

    add weego & ring

--- components/Applinks.vue
+++ components/Applinks.vue
@@ -22,6 +22,27 @@
         広大なキャンパスの地図。キャンパス交通システムやTXの時刻表も。
       </p>
     </div>
+    <div id="weego">
+      <h2 class="title">ゆるぽSNS Weego</h2>
+      <ul>
+        <li><a href="https://weego.fun/">公式サイト</a></li>
+        <li><a href="https://apps.apple.com/app/weego-fun-%E3%82%86%E3%82%8B%E3%81%BCsns-%E3%82%A6%E3%82%A3%E3%83%BC%E3%82%B4/id1588088704">AppStore</a></li>
+        <li><a href="https://play.google.com/store/apps/details?id=com.flutterflow.weego">Google Play</a></li>
+      </ul>
+      <p>
+        予定募集SNS。
+      </p>
+    </div>
+    <div class="ring">
+      <h2 class="title">Ring</h2>
+      <ul>
+        <li><a href="https://apps.apple.com/app/ring-%E3%82%B3%E3%83%9F%E3%83%A5%E3%83%8B%E3%83%86%E3%82%A3%E3%81%A8%E7%B9%8B%E3%81%8C%E3%82%8B/id1588422998">AppStore</a></li>
+        <li><a href="https://play.google.com/store/apps/details?id=com.tsukuba.community">Google Play</a></li>
+      </ul>
+      <p>
+        筑波大学のサークル活動について知ることができるアプリ。パソコンを使用している人には<a href="https://www.stb.tsukuba.ac.jp/~shinkan-web/">新歓Web</a>も役立つ。
+      </p>
+    </div>
   </div>
 </template>
 
--- pages/appli.vue
+++ pages/appli.vue
@@ -6,7 +6,18 @@
 </template>
 
 <script lang="ts">
-
+export default {
+  data () {
+    return {
+      title: '筑波大学関連アプリ集'
+    }
+  },
+  head () {
+    return {
+      title: '筑波大学関連アプリ集'
+    }
+  }
+}
 </script>
 
 <style>
