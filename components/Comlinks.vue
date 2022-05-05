f078c0a9e7106f1ec233a410877c754faae297a2
Osumi Akari <kig.osumiakari@gmail.com>
2022-05-05 14:17:09 +0900

    add comlinks

---
+++ components/Comlinks.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="Comlinks">
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile is-parent py-0">
+        <a class="py-6 tile is-child is-4 zdk" href="https://www.stb.tsukuba.ac.jp/~zdk/home/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  全代会
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 meikei" href="https://www.meikei.or.jp/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  茗渓会
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 shihou" href="https://futureship.sec.tsukuba.ac.jp/shihoukai/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  紫峰会基金
+              </p>
+            </div>
+        </a>
+      </div>
+      <div class="tile is-parent py-0">
+        <a class="py-6 tile is-child is-4 souhou" href="https://www.sohosai.com/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  雙峰祭
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 yadosai" href="https://yadosai.com/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  やどかり祭
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 spoday" href="https://www.spoday.info/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  スポデー
+              </p>
+            </div>
+        </a>
+      </div>
+      <div class="tile is-parent py-0">
+        <a class="py-6 tile is-child is-4 bunsaren" href="https://bunsastaff.wixsite.com/mysite-3">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  文サ連
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 geisaren" href="https://www.stb.tsukuba.ac.jp/~geisa/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  芸サ連
+              </p>
+            </div>
+        </a>
+        <a class="py-6 tile is-child is-4 taiikukai" href="https://www.stb.tsukuba.ac.jp/~taiikukai/">
+            <div class="has-text-white-ter is-size-3 has-text-centered">
+              <p>
+                  体育会
+              </p>
+            </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .zdk {
+    background-color: #1A237E;
+  }
+  .meikei {
+    background-color: #2E3092;
+  }
+  .shihou {
+    background-color: #70b5c7;
+  }
+  .souhou {
+    background-color: #EB6D1F; 
+  }
+  .yadosai {
+    background-color: #89cfeb;
+  }
+  .spoday {
+    background-color: #9900CC;
+  }
+  .bunsaren {
+    background-color: #FF4040;
+  }
+  .geisaren {
+    background-color: #99cc00;
+  }
+  .taiikukai {
+    background-color: #000631;
+  }
+</style>
---
+++ pages/com.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+    <Conlinks />
+    <foot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data () {
+    return {
+      title: '委員会等'
+    }
+  },
+  head () {
+    return {
+      title: '委員会等'
+    }
+  }
+}
+</script>
+
+<style>
+@import url(https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css);
+</style>
