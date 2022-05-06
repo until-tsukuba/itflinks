1a1714cf606073d11190be6670db0802160a4396
Osumi Akari <kig.osumiakari@gmail.com>
2022-05-06 14:25:52 +0900

    add tulips search

---
+++ pages/tulipssearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+    <Tulipssearchxml />
+    <foot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data () {
+    return {
+      title: '「Tulips Search」を検索バーへ追加する'
+    }
+  },
+  head () {
+    return {
+      title: '「Tulips Search」を検索バーへ追加する'
+    }
+  }
+}
+</script>
+
+<style>
+@import url(https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css);
+</style>
